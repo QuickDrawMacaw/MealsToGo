a80f90f30157569956c1c623b7e1391e602f0002
Yuki Tabata <ytabata11@gmail.com>
2022-06-21 13:35:28 +1000

    Installed React navigation and the bottom tab navigation. Safe Area Component created to serve as a wrapper for our new screens map and settings

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import { ThemeProvider } from "styled-components";
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "./src/features/restaurants/components/utility/safe-area.component";
 
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -25,7 +43,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Settings" component={Settings} />
+            <Tab.Screen name="Map" component={Map} />
+          </Tab.Navigator>
+        </NavigationContainer>
         <ExpoStatusBar style="auto" />
       </ThemeProvider>
     </>
