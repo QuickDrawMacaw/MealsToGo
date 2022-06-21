11b99bf504c002c30e0154184b03218a7b7acf73
Yuki Tabata <ytabata11@gmail.com>
2022-06-21 22:44:34 +1000

    Restored restaurantsRequest function in restaurants.service.js

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,6 +1,16 @@
 import { mocks } from "./mock";
 import camelize from "camelize";
 
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+    if (!mock) {
+      reject("not found");
+    }
+    resolve(mock);
+  });
+};
+
 const restaurantsTransform = ({ result = [] }) => {
   const mappedResults = results.map((restaurant) => {
     return {
