6d0bb9041f3b37eb6dd0dfacd8a96f723ccaa0e5
Yuki Tabata <ytabata11@gmail.com>
2022-06-23 01:40:16 +1000

    Login functionality works, registered users can login

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -6,17 +6,19 @@ import {
   AccountContainer,
   AccountCover,
   AuthButton,
+  Title,
 } from "../components/account.styles";
 
 export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthButton
           icon="lock-open-outline"
           mode="contained"
-          onPress={() => navigation.navigate("Register")}
+          onPress={() => navigation.navigate("Login")}
         >
           Login
         </AuthButton>
